5481e41e9b2672203c84c30cf19d44f003f35194
Marc-Arthur Saint louis <ms4050@scarletmail.rutgers.edu>
2026-04-27 10:59:23 -0400

    added javadoc for util.Methods

--- app/src/main/java/com/example/project5/util/Methods.java
+++ app/src/main/java/com/example/project5/util/Methods.java
@@ -4,17 +4,30 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.widget.Toast;
 
-import com.google.android.material.dialog.MaterialAlertDialogBuilder;
-
 import java.text.DecimalFormat;
 
+/**
+ * Class that allows to reuse some methods we use very often.
+ * @author Marc-Arthur Saint Louis and Lamita Farroukh.
+ */
 public class Methods {
     public static DecimalFormat priceFormat =  new DecimalFormat();
+
+    /**
+     * Method used to create a toast and display its content.
+     * @param context The context of the view.
+     * @param message The message we want to display.
+     */
     public static void display(Context context, String message)
     {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
     }
 
+    /**
+     * Method used to create an alert dialog and display its content.
+     * @param context The context of the view.
+     * @param message The message we want to display.
+     */
     public static void displayError(Context context, String message)
     {
         AlertDialog.Builder dialog =  new AlertDialog.Builder(context);
